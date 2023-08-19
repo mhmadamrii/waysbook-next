7293f9b3edfde01e8880ad30b36c6f70a3aa8411
mhmadamrii <interceptorghost4@gmail.com>
2023-08-19 16:58:21 +0700

    add tracking

--- src/app/authentication/page.js
+++ src/app/authentication/page.js
@@ -2,6 +2,11 @@ export default function Authentication() {
   return (
     <>
       <h1>Authentication</h1>
+      <h1>
+        Proident duis magna excepteur cupidatat esse officia cillum dolore velit et culpa quis exercitation. Consectetur occaecat officia consequat exercitation cillum duis eu excepteur Lorem. Velit culpa mollit sunt mollit ea labore amet
+        officia reprehenderit in non. Ipsum amet non sint consectetur velit dolor cupidatat minim incididunt in ex. Lorem laboris nulla ad ad quis esse ipsum consectetur et ad aliqua.
+      </h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </>
   );
 }
